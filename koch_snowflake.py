5e4ccbfbe91c126fc6d22dab39a9f8dee90a0b9b
Sergey Chirkov <s.chirkov1@g.nsu.ru>
2025-11-18 19:06:55 +0700

    Update and rename koch_snowflake to koch_snowflake.py

--- koch_snowflake.py
+++ koch_snowflake.py
@@ -1,16 +1,17 @@
 from turtle import *
 
+
 def koch(order, size):
     if order == 0:
         forward(size)
     else:
-        koch(order-1, size/3)
+        koch(order - 1, size / 3)
         left(60)
-        koch(order-1, size/3)
+        koch(order - 1, size / 3)
         right(120)
-        koch(order-1, size/3)
+        koch(order - 1, size / 3)
         left(60)
-        koch(order-1, size/3)
+        koch(order - 1, size / 3)
 
 
 def koch_snowflake(order, size):
@@ -28,12 +29,11 @@ def koch_snowflake(order, size):
 
 def main():
     up()
-    goto(-100,0)
+    goto(-100, 0)
     down()
     n = int(input('Глубина рекурсии:'))
     a = int(input('Длина стороны:'))
     koch_snowflake(n, a)
-
 
 
 if __name__ == '__main__':
