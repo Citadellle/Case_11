f22f5bf217f53b39df52a1db617de8712b9b14d3
Mikhail Gordeev <miha_gord@mail.ru>
2025-11-25 20:46:46 +0700

    Update Koch_curve.py

--- Koch_curve.py
+++ Koch_curve.py
@@ -1,4 +1,5 @@
 import turtle as t
+import ru_local as ru
 
 
 def koch(size: float, order: int) -> None:
@@ -14,10 +15,36 @@ def koch(size: float, order: int) -> None:
         koch(size / 3, order - 1)
 
 
-def main() -> None:
+def main():
+    n = int(input(ru.DEEP))
+    
+    tracer_val = None
+    while tracer_val not in [0, 1]:
+        try:
+            tracer_val = int(input(ru.ANIMATION))
+            if tracer_val not in [0, 1]:
+                print(ru.VALUE_ERROR)
+        except:
+            print(ru.VALUE_ERROR)
+
+    if tracer_val == 1:
+        # Disabling the drawing animation, not updating the screen
+        t.tracer(0)
+    else:
+        t.speed(400)
+
     t.up()
-    t.goto(-100, 0)
+    t.goto(-500, 0)
     t.down()
-    n = int(input('Глубина рекурсии:'))
-    a = int(input('Длина стороны:'))
-    koch(a, n)
+
+    koch(1000, n)
+
+    # Hide the turtle cursor
+    t.hideturtle()
+    # Updating the turtle window
+    t.update()
+    t.done()
+
+
+if __name__ == '__main__':
+    main()
