768bf49bc33b7e4a1aba750c03b60f946d81efee
Mikhail Gordeev <miha_gord@mail.ru>
2025-11-25 20:49:04 +0700

    Update Tree_fractal.py

--- Tree_fractal.py
+++ Tree_fractal.py
@@ -1,58 +1,50 @@
 import turtle as t
+import ru_local as ru
 
-def tree(a: float, n: int) -> None:
-    '''
-    This function starts a recursive process with a depth of n:
-    1.  The base of recursion is a segment of long a
-    2.  Subsequent levels of recursion - 2 shorter segments
-        extending from the end of the segments of the previous
-        level at angles of 30 degrees
-    3.  At the end of the program, it displays the
-        "Ice Fractal" pattern on the screen.
-    :param a:
-    :param n:
-    :return None:
-    '''
-    if n==0:
-        t.fd(a)
-        t.fd(-a)
+
+def spiral(size, order):
+    if order == 0:
+        t.forward(size)
     else:
-        t.fd(3*a/4)
-        t.rt(30)
-        tree(2*a/3,n-1)
-        t.lt(60)
-        tree(2*a/3,n-1)
-        t.rt(30)
-        t.fd(-3*a/4)
+        spiral(size / 2, order - 1)
+        t.right(90)
+        spiral(size / 3, order - 1)
+        t.right(90)
+        spiral(size / 4, order - 1)
+        t.right(90)
+        spiral(size / 5, order - 1)
 
 
 def main():
-    n = int(input('Введите глубину рекурсии: '))
+    n = int(input(ru.DEEP))
     
     tracer_val = None
     while tracer_val not in [0, 1]:
         try:
-            tracer_val = int(input('Отключить анимацию рисования или оставить? (1 - отключить; 0 - оставить) '))
+            tracer_val = int(input(ru.ANIMATION))
             if tracer_val not in [0, 1]:
-                print('Введите корректное значение')
+                print(ru.VALUE_ERROR)
         except:
-            print('Введите корректное значение')
+            print(ru.VALUE_ERROR)
 
     if tracer_val == 1:
-        # Отключение анимации рисования, не обновление экрана
+        # Disabling the drawing animation, not updating the screen
         t.tracer(0)
     else:
         t.speed(400)
 
     t.up()
-    t.goto(-500, 0)
+    t.goto(-500, 250)
     t.down()
 
-    t.left(90)
-    tree(100, n)
+    spiral(1000, n)
 
-    # Спрятать курсор черепахи
+    # Hide the turtle cursor
     t.hideturtle()
-    # Обновление окна с черепахой
+    # Updating the turtle window
     t.update()
     t.done()
+
+
+if __name__ == '__main__':
+    main()
