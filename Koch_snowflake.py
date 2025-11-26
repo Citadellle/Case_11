6c0081e57b7cc96f20d76194ebe835691ba671f4
Mikhail Gordeev <miha_gord@mail.ru>
2025-11-26 08:52:37 +0700

    Update Koch_snowflake.py

--- Koch_snowflake.py
+++ Koch_snowflake.py
@@ -65,7 +65,7 @@ def main() -> None:
     Returns:
         None
     '''
-    n = int(input(ru.DEEP))
+    n = int(input(ru.DEPTH))
     
     tracer_val = None
     while tracer_val not in [0, 1]:
